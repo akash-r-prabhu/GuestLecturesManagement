4ad1b6000c17471924426481a325d66e7052f64d
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-14 02:27:49 +0530

    frontend

--- geust lecture management front end/public/ljava.js
+++ geust lecture management front end/public/ljava.js
@@ -1,18 +1,12 @@
 function validateLogin(event) {
     event.preventDefault();
-  
-    // Get the username and password values
-    var username = document.getElementById('username').value;
+      var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-  
-    // Validate the credentials and determine the group
-    var group = validateCredentials(username, password);
+      var group = validateCredentials(username, password);
   
     if (group === 'admin') {
-      // Redirect admin to the admin dashboard
-      window.location.href = 'index.html';
+      window.location.href = 'admindashboard.html';
     } else if (group === 'user') {
-      // Redirect user to the user dashboard
       window.location.href = 'index.html';
     } else {
       alert('Invalid credentials. Please try again.');
@@ -20,8 +14,6 @@ function validateLogin(event) {
   }
   
   function validateCredentials(username, password) {
-    // Replace this with your own logic to validate the credentials and determine the group
-    // For example, compare with predefined username and password for admin and user
     var adminUsername = 'admin';
     var adminPassword = 'admin123';
     var userUsername = 'user';
